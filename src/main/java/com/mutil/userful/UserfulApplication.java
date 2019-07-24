9454276afa50aa3495a64191e78841dd96b45959
朱良 <zhuliang@ule.com>
2019-07-24 15:17:03 +0800

    zl-strategy 模式

--- src/main/java/com/mutil/userful/UserfulApplication.java
+++ src/main/java/com/mutil/userful/UserfulApplication.java
@@ -3,11 +3,13 @@ package com.mutil.userful;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan("com.mutil.userful")
 //@EnableEncryptableProperties //开启配置文件加密功能jasypt
+@EnableDiscoveryClient
 public class UserfulApplication {
 
 	public static void main(String[] args) {
